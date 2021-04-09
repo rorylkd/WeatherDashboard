494a08eda1b47a86a310a5e8892515be16414569
rorylkd <rory.donaldson14@gmail.com>
2021-04-09 20:01:57 +0800

    Sidebar html & CSS. JS script dealing with creating buttons based on user input

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-// First commit
+// Event listener for search button
+document.getElementById("searchBtn").addEventListener("click", writeBtn);
+
+// Function to create buttons with the value of whatever was typed in the search bar
+function writeBtn(){
+const inputBoxEl = document.getElementById("inputBox"); //Selectors
+const searchListEl = document.getElementById("searchList"); 
+
+const newButtonText = inputBoxEl.value; // New variable based on the string typed into the input box
+
+searchListEl.insertAdjacentHTML('afterend', '<button class="searchres">'+ newButtonText +'</button>'); // Creates button elements under the search list element and gives them newButtonText
+}
+
+
+
+
+
+
