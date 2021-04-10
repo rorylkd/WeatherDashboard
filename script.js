5037dae29ae5e53957bbbc659dc7990bc676d6bc
rorylkd <rory.donaldson14@gmail.com>
2021-04-10 20:06:17 +0800

    Added fetch method to return current weather. And some CSS stuff because I apparently can't just leave it alone

--- script.js
+++ script.js
@@ -6,10 +6,31 @@ function writeBtn(){
 const inputBoxEl = document.getElementById("inputBox"); //Selectors
 const searchListEl = document.getElementById("searchList"); 
 
-const newButtonText = inputBoxEl.value; // New variable based on the string typed into the input box
+const cityName = inputBoxEl.value; // New variable based on the string typed into the input box
+if(cityName!==""){ // Doesn't make a button if the string is empty
+searchListEl.insertAdjacentHTML('afterend', '<button class="searchres">'+ cityName +'</button>'); // Creates button elements under the search list element and gives them cityName
 
-searchListEl.insertAdjacentHTML('afterend', '<button class="searchres">'+ newButtonText +'</button>'); // Creates button elements under the search list element and gives them newButtonText
-}
+// Calling the Weather API
+
+var cityCurrentWeatherUrl = 'http://api.openweathermap.org/data/2.5/weather?q='+cityName+'&appid=b5d4186b9b4d74b711271811c8313863';
+var cityFiveDayWeatherUrl = 'http://api.openweathermap.org/data/2.5/forecast?q='+cityName+'&appid=b5d4186b9b4d74b711271811c8313863';
+
+ console.log(cityCurrentWeatherUrl);
+ console.log(cityFiveDayWeatherUrl);
+
+ fetch(cityCurrentWeatherUrl) // Given a valid city name, returns object with current weather conditions.
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(weatherData){
+        console.log(weatherData);
+    });
+
+
+}}
+
+
+
 
 
 
