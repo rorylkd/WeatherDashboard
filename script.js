d7dc2ec13537f6172c31d3943a9af757afc43be1
rorylkd <rory.donaldson14@gmail.com>
2021-04-12 14:40:51 +0800

    Added UVindex bullshit

--- script.js
+++ script.js
@@ -24,13 +24,10 @@ var cityFiveDayWeatherUrl = 'http://api.openweathermap.org/data/2.5/forecast?q='
     })
     .then(function(weatherData){
         console.log(weatherData);
-        console.log(weatherData.name);
-        console.log(weatherData.main.temp);
+        
 
     const currentTemp = weatherData.main.temp - 273.15 // Temp is given in degrees kelvin, need celsius. 
-    console.log(currentTemp);
     roundedCurrentTemp = Math.round(currentTemp*100)/100; //Rounding our temp to two decimal places
-    console.log(roundedCurrentTemp);
 
     // More Selectors
 
@@ -53,7 +50,32 @@ var cityFiveDayWeatherUrl = 'http://api.openweathermap.org/data/2.5/forecast?q='
     windEl.innerHTML = "Wind: " + weatherData.wind.speed + "m/s";
     humidityEl.innerHTML = "Humidity: " + weatherData.main.humidity + "%";
     iconEl.src = "http://openweathermap.org/img/wn/"+weatherData.weather[0].icon+"@2x.png";
-    // tempEl.innerHTML = "UV Index: " + ;
+    
+
+    // To get UV index, i need long & lat of a city as the API I'd need to use doesn't take city name as a parameter.
+    // To get long+lat of a city, I can look it up using the data used above(ex. weatherData.coord.lat & weatherData.coord.long). 
+    // I need to use that data to build the URL needed to call the other API (the One Call API, as the UV Index API is retired since April 1st 2021)
+    
+
+    var longitude = weatherData.coord.lon;
+    var latitude = weatherData.coord.lat;
+
+    var OneCallAPIUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat='+latitude+'&lon='+longitude+'&exclude={part}&appid=b5d4186b9b4d74b711271811c8313863';
+
+
+    
+    fetch(OneCallAPIUrl)
+        .then(function(response2){
+            return response2.json();
+
+        })
+        .then(function(OneCallData){
+            console.log(OneCallData);
+            UVindexEl.innerHTML = "UV Index: " + OneCallData.current.uvi;
+
+        });
+    
+    
     
     
     
@@ -66,6 +88,12 @@ var cityFiveDayWeatherUrl = 'http://api.openweathermap.org/data/2.5/forecast?q='
 
 
     
+
+
+
+
+
+
 }}
 
 
