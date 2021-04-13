207c1d07e27018b8ee30621530dd98f7913911d7
rorylkd <rory.donaldson14@gmail.com>
2021-04-13 12:49:19 +0800

    Added UVIndex color scale, added 5day forecast icon, temp, wind & humidity

--- script.js
+++ script.js
@@ -30,13 +30,46 @@ var cityFiveDayWeatherUrl = 'http://api.openweathermap.org/data/2.5/forecast?q='
     roundedCurrentTemp = Math.round(currentTemp*100)/100; //Rounding our temp to two decimal places
 
     // More Selectors
-
+    // Current Day
     const cityNameEl = document.getElementById("cityName");
     const tempEl = document.getElementById("temp");
     const windEl = document.getElementById("wind");
     const humidityEl = document.getElementById("humidity");
     const UVindexEl = document.getElementById("UVindex");
     const iconEl = document.getElementById("icon");
+
+    // Day 1
+    const temp1El = document.getElementById("day1Temp");
+    const wind1El = document.getElementById("day1Wind");
+    const humidity1El = document.getElementById("day1Humidity");
+    const icon1El = document.getElementById("day1Icon");
+    //Day 2
+    const temp2El = document.getElementById("day2Temp");
+    const wind2El = document.getElementById("day2Wind");
+    const humidity2El = document.getElementById("day2Humidity");
+    const icon2El = document.getElementById("day2Icon");
+    //Day 3
+    const temp3El = document.getElementById("day3Temp");
+    const wind3El = document.getElementById("day3Wind");
+    const humidity3El = document.getElementById("day3Humidity");
+    const icon3El = document.getElementById("day3Icon");
+    //Day 4
+    const temp4El = document.getElementById("day4Temp");
+    const wind4El = document.getElementById("day4Wind");
+    const humidity4El = document.getElementById("day4Humidity");
+    const icon4El = document.getElementById("day4Icon");
+    //Day 5
+    const temp5El = document.getElementById("day5Temp");
+    const wind5El = document.getElementById("day5Wind");
+    const humidity5El = document.getElementById("day5Humidity");
+    const icon5El = document.getElementById("day5Icon");
+    
+    
+    
+    
+    
+    
+    
     
     // Date stuff goes here
     const dateEl = document.getElementById("date");
@@ -73,7 +106,8 @@ var cityFiveDayWeatherUrl = 'http://api.openweathermap.org/data/2.5/forecast?q='
     
 
     // To get UV index, i need long & lat of a city as the API I'd need to use doesn't take city name as a parameter.
-    // To get long+lat of a city, I can look it up using the data used above(ex. weatherData.coord.lat & weatherData.coord.lon). 
+    // To get long+lat of a city, I can look it up using the data used above(ex. weatherData.coord.lat & weatherData.coord.lon).
+    // I'll also use the One Call API to grab the daily forcasts. Could have used it earlier but I'd still need to call the Current Weather one 
     
     
 
@@ -91,7 +125,58 @@ var cityFiveDayWeatherUrl = 'http://api.openweathermap.org/data/2.5/forecast?q='
         })
         .then(function(OneCallData){
             console.log(OneCallData);
-            UVindexEl.innerHTML = "UV Index: " + OneCallData.current.uvi;
+            
+            
+            
+            
+            UVindexEl.innerHTML = "UV Index: " + OneCallData.current.uvi; //Current UV Index
+            
+            
+            if((OneCallData.current.uvi) <= 3){ //Changes background color depending on UV Index. 
+                UVindexEl.style.backgroundColor = "green";
+            }else if((OneCallData.current.uvi)<= 7){
+                UVindexEl.style.backgroundColor = "orange";
+            }else{UVindexEl.style.backgroundColor = "red";}
+
+             var day1TempCelcius = OneCallData.daily[0].temp.day - 273.15; //Changing all the temps to celsius. 
+             var day2TempCelcius = OneCallData.daily[1].temp.day - 273.15;
+             var day3TempCelcius = OneCallData.daily[2].temp.day - 273.15;
+             var day4TempCelcius = OneCallData.daily[3].temp.day - 273.15;
+             var day5TempCelcius = OneCallData.daily[4].temp.day - 273.15;
+             
+
+            temp1El.innerHTML ="Temp: "+ day1TempCelcius +"°C"; //Adding my elements based on the API data.
+            wind1El.innerHTML = "Wind: "+ OneCallData.daily[0].wind_speed + "m/s";
+            humidity1El.innerHTML = "Humidity: "+ OneCallData.daily[0].humidity + "%";
+           icon1El.src = "http://openweathermap.org/img/wn/"+ OneCallData.daily[0].weather[0].icon +"@2x.png";
+           icon1El.style.display = "block"; //Making my img visible
+
+           temp2El.innerHTML ="Temp: "+ day2TempCelcius +"°C";
+            wind2El.innerHTML = "Wind: "+ OneCallData.daily[1].wind_speed + "m/s";
+            humidity2El.innerHTML = "Humidity: "+ OneCallData.daily[1].humidity + "%";
+            icon2El.src = "http://openweathermap.org/img/wn/"+ OneCallData.daily[1].weather[0].icon +"@2x.png";
+            icon2El.style.display = "block";
+
+            temp3El.innerHTML ="Temp: "+ day3TempCelcius +"°C";
+            wind3El.innerHTML = "Wind: "+ OneCallData.daily[2].wind_speed + "m/s";
+            humidity3El.innerHTML = "Humidity: "+ OneCallData.daily[2].humidity + "%";
+            icon3El.src = "http://openweathermap.org/img/wn/"+ OneCallData.daily[2].weather[0].icon +"@2x.png";
+            icon3El.style.display = "block";
+
+            temp4El.innerHTML ="Temp: "+ day4TempCelcius +"°C";
+            wind4El.innerHTML = "Wind: "+ OneCallData.daily[3].wind_speed + "m/s";
+            humidity4El.innerHTML = "Humidity: "+ OneCallData.daily[3].humidity + "%";
+            icon4El.src = "http://openweathermap.org/img/wn/"+ OneCallData.daily[3].weather[0].icon +"@2x.png";
+            icon4El.style.display = "block";
+
+            temp5El.innerHTML ="Temp: "+ day5TempCelcius +"°C";
+            wind5El.innerHTML = "Wind: "+ OneCallData.daily[4].wind_speed + "m/s";
+            humidity5El.innerHTML = "Humidity: "+ OneCallData.daily[4].humidity + "%";
+            icon5El.src = "http://openweathermap.org/img/wn/"+ OneCallData.daily[4].weather[0].icon +"@2x.png";
+            icon5El.style.display = "block";
+
+
+
 
         });
     
